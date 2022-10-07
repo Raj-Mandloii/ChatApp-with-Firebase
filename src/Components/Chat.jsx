a9c97f58b94a232ab5584adc53ad1085c3b35e26
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-10-07 11:43:50 +0530

    chat ui added

--- src/Components/Chat.jsx
+++ src/Components/Chat.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
-
+import add from "../assets/plus.png"
+import camera from "../assets/camera.png"
+import more from "../assets/more.png"
 export const Chat = () => {
   return (
-    <div className='chat'>Chat</div>
+    <div className='chat'>
+      <div className='chatInfo'>
+        <span>Raj</span>
+        <div className='chatIcons'>
+          <img src={camera} alt='Camera'/>
+          <img src={add} alt='Add'/>
+          <img src={more} alt=''/>
+        </div>
+      </div>
+    </div>
   )
 }
