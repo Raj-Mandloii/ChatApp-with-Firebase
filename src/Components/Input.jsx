94e6e710cd2641b0f7270b0f95783910543cc5af
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-10-07 12:34:50 +0530

    input UI added

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import openFile from "../assets/openFile.png"
+import attach from "../assets/attach.png"
 export const Input = () => {
   return (
-    <div>Input</div>
+    <div className='input'>
+      <input type="text" placeholder='Type Something .....'/>
+      <div className="send"></div>
+      <img src={attach} alt="" />
+      <input type="file" style={{display:"none"}} id="file"/>
+      <label htmlFor="file">
+        <img src={openFile} alt="" />
+      </label>
+      <button>Send</button>
+    </div>
   )
 }
