3a44a955ac1881b596b5f6920814b720e2358873
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-10-07 19:18:28 +0530

    login functionalities added

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,27 @@
-import React from 'react'
-import img from "../assets/addImage.png"
+import React, { useState } from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+import { useNavigation } from 'react-router-dom';
+import { auth } from '../firebase';
 export const Login = () => {
+    const navigate = useNavigation()
+    const [error, setErr] = useState(false)
+    const handleSubmit = async (e) => {
+        // setLoading(true);
+        e.preventDefault();
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+
+        try {
+            await createUserWithEmailAndPassword(auth, email, password)
+            navigate('/')
+        } catch (err) {
+            setErr(true);
+            //   setLoading(false);
+        }
+    };
+
+
     return (
         <div>
             <div className='formContainer'>
@@ -8,10 +29,12 @@ export const Login = () => {
                 <div className='formWrapper'>
                     <span className="logo">Chat With Firebase</span>
                     <span className="title">Login</span>
-                    <form action=''>
+                    <form onSubmit={handleSubmit}>
                         <input type="email" placeholder="Email" />
                         <input type="password" placeholder="Password" />
                         <button>Sign In</button>
+                        {error && <span>Something went wrong</span>}
+
                     </form>
                     <p>If you don't have account ? Register</p>
                 </div>
