7220371f0d43c188b84317f1a8dd06a8a21bbcfa
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-10-07 13:49:03 +0530

    input and message added

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -5,13 +5,16 @@ export const Input = () => {
   return (
     <div className='input'>
       <input type="text" placeholder='Type Something .....'/>
-      <div className="send"></div>
+      <div className="send">
       <img src={attach} alt="" />
       <input type="file" style={{display:"none"}} id="file"/>
       <label htmlFor="file">
         <img src={openFile} alt="" />
       </label>
       <button>Send</button>
+
+      </div>
+      
     </div>
   )
 }
--- src/Components/Massage.jsx
+++ src/Components/Massage.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export const Massage = () => {
   return (
-    <div>Massage</div>
+    <div className='message'>
+      <div className="messageInfo ">
+        <img src="https://raj-mandloii.github.io/raj-mandloi/static/media/profile.bc02411670caa5d19ea7.jpg" alt="" />
+      </div>
+      <div className="messageContent">
+        <p>Hello</p>
+        <img src="https://raj-mandloii.github.io/raj-mandloi/static/media/profile.bc02411670caa5d19ea7.jpg" alt="" />
+
+      </div>
+    </div>
   )
 }
+ 
