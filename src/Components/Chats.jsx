7eacb0fad575b365acce853b9d8b4c392738fb9f
Raj Mandloii <rajmandloi01.01@gmail.com>
2022-10-08 20:39:03 +0530

    dynamic users added to chat section

--- src/Components/Chats.jsx
+++ src/Components/Chats.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
+import { doc, onSnapshot } from 'firebase/firestore';
+import React, { useEffect } from 'react'
+import { useContext } from 'react';
+import { useState } from 'react'
+import { AuthContext } from '../context/AuthContext';
+import { db } from '../firebase';
 
 export const Chats = () => {
+  const [chats, setChats] = useState([]);
+  const { currentUser } = useContext(AuthContext)
+  useEffect(() => {
+    const getChats = () => {
+      const unsub = onSnapshot(doc(db, "userChats", currentUser.uid), (doc) => {
+        setChats(doc.data());
+      });
+      return () => {
+        unsub();
+      }
+    }
+    currentUser.uid && getChats();
+  }, [currentUser.uid])
+  console.log(Object.entries(chats))
   return (
     <div className='chats'>
-       <div className="userChat">
-        <img src="https://raj-mandloii.github.io/raj-mandloi/static/media/profile.bc02411670caa5d19ea7.jpg" />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello, Raj</p>
+      {Object.entries(chats)?.map((chat) => (
+
+        <div className="userChat" key={chat[0]}>
+          <img src={chat[1].userInfo?.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{chat[1].userInfo?.displayName}</span>
+            <p>{chat[1].userInfo?.lastMessage?.text}</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   )
 }
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,4 +1,6 @@
-import { collection, query, getDocs, where, setDoc, doc, updateDoc, serverTimestamp } from 'firebase/firestore'
+import { collection, query, getDocs,getDoc, where, 
+  setDoc, doc, updateDoc, 
+  serverTimestamp } from 'firebase/firestore'
 import React from 'react'
 import { useState } from 'react'
 import { db } from "../firebase";
@@ -10,54 +12,61 @@ export const Search = () => {
   const [err, setErr] = useState(false);
   const { currentUser } = useContext(AuthContext)
 
-  const handleSeach = async () => {
+  const handleSearch = async () => {
     try {
-      const q = query(collection(db, "users"), where("displayName", "===", username))
+      const q = query(collection(db, "users"), 
+      where("displayName", "==", username))
+
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
-        setUser(doc.data)
+        setUser(doc.data())
       });
     } catch (e) {
       setErr(true)
     }
   }
   const handleKey = (e) => {
-    e.code === "Enter" && handleSeach();
+    e.code === "Enter" && handleSearch();
   }
 
   const handleSelect = async () => {
     // 1st check the chat between 2 people exist or not if not 
     // then create else open it..!
-    const combinedId = currentUser.uid > user.uid ? currentUser + user.uid : user.uid + currentUser.uid;
-    try {
-      const res = await getDocs(db, "chats", combinedId)
-      if (!res.exists()) {
-        await setDoc(doc, (db, "chats", combinedId), {
-          messages: []
-        });
-        await updateDoc(doc(db, "userChats", user.uid), {
-          [combinedId + ".userInfo"]: {
-            uid: user.uid,
-            displayName: user.displayName,
-            photoURL: user.photoURL,
-          },
-          [combinedId + ".date"]: serverTimestamp()
-        });
-        await updateDoc(doc(db, "userChats", currentUser.uid), {
-          [combinedId + ".userInfo"]: {
-            uid: currentUser.uid,
-            displayName: currentUser.displayName,
+    const combinedId =
+    currentUser.uid > user.uid
+      ? currentUser.uid + user.uid
+      : user.uid + currentUser.uid;
+  try {
+    const res = await getDoc(doc(db, "chats", combinedId));
 
-            photoURL: currentUser.photoURL
-          },
-          [combinedId + ".date"]: serverTimestamp()
-        })
-      }
+    if (!res.exists()) {
+      //create a chat in chats collection
+      await setDoc(doc(db, "chats", combinedId), { messages: [] });
 
-    } catch (e) { }
-    setUser(null);
-    setUsername("")
-  }
+      //create user chats
+      await updateDoc(doc(db, "userChats", currentUser.uid), {
+        [combinedId + ".userInfo"]: {
+          uid: user.uid,
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+        },
+        [combinedId + ".date"]: serverTimestamp(),
+      });
+
+      await updateDoc(doc(db, "userChats", user.uid), {
+        [combinedId + ".userInfo"]: {
+          uid: currentUser.uid,
+          displayName: currentUser.displayName,
+          photoURL: currentUser.photoURL,
+        },
+        [combinedId + ".date"]: serverTimestamp(),
+      });
+    }
+  } catch (err) {}
+
+  setUser(null);
+  setUsername("")
+};
   return (
     <div className='search'>
       <div className='searchForm'>
